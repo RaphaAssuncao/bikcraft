da1f9397647c4efd1c8b421499bad52aea7cd30d
raphaAssuncao <raphassuncao.code@gmail.com>
2026-03-20 21:04:42 -0400

    parametros URL

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
+//Ativar Links no Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLinks(link) {
   const url = location.href;
-
   const href = link.href;
 
-  url.includes;
   if (url.includes(href)) {
     link.classList.add("ativo");
   }
 }
 
 links.forEach(ativarLinks);
+
+// Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
